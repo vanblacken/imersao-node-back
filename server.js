8acb29054230fe9a3b298d9531d5bf6a18a780db
joffison <vanblacken@gmail.com>
2024-11-25 21:43:21 -0300

    Primeiro commit do projeto

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import routes from "./src/routes/postRoutes.js";
+
+const app = express();
+routes(app);
+
+app.listen(3000, () => {
+    console.log("Servidor escutando ...")
+});
+
+
